96a9d4c1c0985df4dfd6df72d14ed68416e4acdc
Nathan <nathanansantos@gmail.com>
2024-05-22 14:12:26 -0300

    Primeiro commit

---
+++ DataContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ControleDeMembros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeMembros.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<MembroModel> Membros { get; set; }
+    }
+}
---
+++ Models/MembroModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeMembros.Models
+{
+    public class Membros
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        [Display(Name = "Data de nascimento")]
+        public DateTime DataNasc { get; set; }
+        public string Contato { get; set; }
+        public string Endereço { get; set; }
+    }
+}
